417a878d1749806bcee14a324e22ecc661ccf758
blonded04 <generalretcher@gmail.com>
2022-01-16 04:38:15 +0300

    Nothing works, aborting idea :)

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,26 @@ use cppanywhere::ThreadPool;
 use std::fs;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::process::Command;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:2004").unwrap();
+    let pool = ThreadPool::new(8);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(|| handle_connection(stream));
+    }
+
+    println!("Shutting down.");
+}
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
     let (status_line, filename) = if buffer.starts_with(b"GET / HTTP/1.1\r\n") {
+        run_binary("main");
         ("HTTP/1.1 200 OK", "html/markup.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "html/404.html")
@@ -25,14 +39,10 @@ fn handle_connection(mut stream: TcpStream) {
     stream.flush().unwrap();
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:2004").unwrap();
-    let pool = ThreadPool::new(8);
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        pool.execute(|| handle_connection(stream));
+fn run_binary(filename: &str) {
+    if cfg!(target_os = "windows") {
+        Command::new(format!(".\\cpp\\{}", filename)).spawn().expect("No file.");
+    } else {
+        Command::new(format!("./cpp/{}", filename)).spawn().expect("No file.");
     }
-
-    println!("Shutting down.");
 }
